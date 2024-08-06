4cc201e356e1b97f1f78f819ecac734302a0fc20
ThankGod Ossaija <codebugsolved@gmail.com>
2024-08-06 05:02:34 +0100

    ready all files in a directory

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
-use std::{fs::File, io, process::exit};
+use std::{fs::{self, File}, io, path::Path};
 
 use xml::{reader::XmlEvent, EventReader};
 
-fn main() {
-    let file_path = "docs.gl/gl4/glClear.xhtml";
-    let content = read_entire_xml_file(file_path).unwrap_or_else(|err| {
-        eprintln!("ERROR: could not read the xml {file_path} : {err}");
-        exit(1);
-    });
-    println!("{content}")
+fn main() -> io::Result<()> {
+    let dir_path = "docs.gl/gl4";
+    let dir = fs::read_dir(dir_path)?;
+
+    for file in dir  {
+       let  file_path = file?.path();
+        let content = read_entire_xml_file(&file_path)?;
+        println!("{file_path:?} => {size} ", size = content.len());
+    }
+    Ok(())
 }
 
-fn read_entire_xml_file(file_path: &str) -> io::Result<String> {
+fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
     let mut content = String::new();
     let file = File::open(file_path)?;
-
    let er = EventReader::new(file);
-
    for event in er.into_iter() {
     let event = event.expect("TODO");
     if let XmlEvent::Characters(text ) = event {
         content.push_str(&text);
-        print!("{text}");
     }
 }
 Ok(content)
