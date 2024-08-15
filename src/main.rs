a2d0c0f33d7787eec21bd699081df4a40c4df717
ThankGod Ossaija <codebugsolved@gmail.com>
2024-08-15 02:11:29 +0100

    document ranking

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ impl <'a> Lexer<'a> {
         }
     }
 
-    fn collect_words(&mut self) -> Option<&'a[char]>  {
+    fn collect_words(&mut self) -> Option<String>  {
         let mut n = 0;
         while n < self.content.len() && self.content[n].is_alphanumeric() {
             n += 1;
@@ -25,7 +25,7 @@ impl <'a> Lexer<'a> {
        self.chop(n)
     }
 
-    fn collect_numbers(&mut self) -> Option<&'a[char]> {
+    fn collect_numbers(&mut self) -> Option<String> {
         let mut n = 0;
         while n < self.content.len() && self.content[n].is_numeric() {
             n += 1;
@@ -33,29 +33,29 @@ impl <'a> Lexer<'a> {
         self.chop(n)
     }
 
-    fn chop(&mut self, n: usize) -> Option<&'a[char] >{
-        let token = &self.content[0..n];
+    fn chop(&mut self, n: usize) -> Option<String>{
+        let token = &self.content[0..n].iter().collect::<String>();
         self.content = &self.content[n..];
-        Some(token)
+        Some(token.to_owned())
     }
 
-    fn next_token(&mut self) -> Option<&'a[char]> {
+    fn next_token(&mut self) -> Option<String> {
         self.trim_left();
         if self.content.len() == 0 {
             return None;
         }
         if self.content[0].is_numeric() {
-            return self.collect_numbers()
+            return Some(self.collect_numbers().iter().map(|x| x.to_ascii_uppercase()).collect());
         }
         if self.content[0].is_alphabetic() {
-            return self.collect_words()
+            return Some(self.collect_words().iter().map(|x| x.to_ascii_uppercase()).collect());
         }
         self.chop(1)
     }
 }
 
 impl<'a> Iterator for Lexer<'a> {
-    type Item = &'a[char];
+    type Item = String;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.next_token()
@@ -66,17 +66,15 @@ type TermFreq = HashMap::<String, usize>;
 type TermFreqIndex = HashMap<PathBuf, TermFreq>;
 
 fn main() -> io::Result<()> {
-    main2()?;
-
     let index_path = "index.json";
    let index_file =  File::open(index_path)?;
    println!("Reading {index_path} index file...");
    let tf_index: TermFreqIndex = serde_json::from_reader(index_file).expect("reading file to TermFreqIndex successful");
-   println!("{index_path} contains {count} files", count= tf_index.len());
+    search("bind, to buffer".to_string(), tf_index);
    Ok(())
 }
 
-fn main2() -> io::Result<()> {
+fn index_folder() -> io::Result<()> {
     let dir_path = "docs.gl/gl4";
     let dir = fs::read_dir(dir_path)?;
     let mut tf_index = TermFreqIndex::new();
@@ -90,11 +88,10 @@ fn main2() -> io::Result<()> {
         println!("Indexing {file_path:?}");
         let mut tf = TermFreq::new();
         for token in Lexer::new(&content) {
-            let term = token.iter().map(|x| x.to_ascii_uppercase()).collect::<String>();
-            if let Some(freq) = tf.get_mut(&term) {
+            if let Some(freq) = tf.get_mut(&token) {
                 *freq +=1;
             }else {
-                tf.insert(term, 1);
+                tf.insert(token, 1);
             }
         }
 
@@ -126,4 +123,32 @@ fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
 }
 Ok(content)
 }
+
+fn search(keywords: String, tf_index: TermFreqIndex) {
+    let mut results = Vec::<(PathBuf, f32)>::new();
+    for (path, tf_table) in &tf_index {
+        let mut rank = 0f32;
+        for token in Lexer::new(&keywords.chars().collect::<Vec<_>>()) {
+            rank += tf(&token, &tf_table) * idf(&token, &tf_index);
+        }
+        results.push((path.clone(), rank))
+    }
+    results.sort_by(|(_, rank1), (_, rank2)| rank1.partial_cmp(rank2).unwrap());
+    results.reverse();
+    for (path, rank) in results {
+        println!("{path} => {rank}", path = path.display());
+    }
+}
+
+fn tf(term: &str, doc: &TermFreq) -> f32 {
+    let numerator = doc.get(term).cloned().unwrap_or(0) as f32;
+    let denomitor = doc.iter().map(|(_, f)| *f).sum::<usize>() as f32;
+    numerator / denomitor
+ }
+
+ fn idf(t :&str, d: &TermFreqIndex) -> f32 {
+    let n = d.len() as f32;
+    let m =  d.values().filter(| tf| tf.contains_key(t)).count().max(1) as f32;
+     (n /  m).log10()
+ }
 
